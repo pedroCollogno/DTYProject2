baea40cd2f2e6091d9ab390ef4d39356fdd5774c
Pierre Averous <piaverous@gmail.com>
2018-11-05 13:49:29 +0100

    First implementation of DBScan on a single station

--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
 import gen.TrackstreamEx_pb2 as ts
 import os
 from progressbar import ProgressBar
+from sklearn.cluster import DBSCAN
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from mpl_toolkits.mplot3d import Axes3D
 
 prp_1 = "prod/s1/TRC6420_ITRProduction_20181026_143235.prp"
 
@@ -70,7 +75,51 @@ def get_track_stream_ex(filepath):
 
 
 tsexs = get_track_stream_ex(prp_1)
+tsex = tsexs[38]
+raw_data = []
 
+for tsex in tsexs:
+    track_stream_data = tsex.data
+    tracks = track_stream_data.tracks
+    print("Found %s tracks in the file.\n" % len(tracks))
 
-def function():
-    return 'lol'
+    for track in tracks:
+        batch = []
+        batch.append(track.itr_measurement.type)
+        batch.append(track.itr_measurement.central_freq_hz)
+        batch.append(track.itr_measurement.bandwidth_hz)
+        batch.append(track.average_azimut_deg)
+
+        # batch.append(track.begin_date.date_ms)
+        # batch.append(track.end_date.date_ms)
+        if batch not in raw_data:
+            raw_data.append(batch)
+    """for batch in raw_data:
+        print("Track data :", batch)"""
+raw_data = np.array(raw_data)
+y_pred = DBSCAN(min_samples=1).fit_predict(raw_data[:, :3])
+
+labels = []
+corresponding_batches = {}
+i = 0
+for label in y_pred:
+    if label not in labels:
+        labels.append(label)
+        corresponding_batches[label] = []
+    corresponding_batches[label].append(raw_data[i])
+    i += 1
+
+fig = plt.figure()
+ax = Axes3D(fig)
+colors = cm.rainbow(np.linspace(0, 1, len(y_pred)))
+print("\nResult of DBScan clustering on input data. There are %s inputs and %s clusters.\n" %
+      (len(raw_data), len(labels)))
+for key in corresponding_batches.keys():
+    print("Label is %s" % key)
+    for batch in corresponding_batches[key]:
+        print("\tTrack data: % s" % batch)
+    corresponding_batches[key] = np.array(corresponding_batches[key])
+    ax.plot(corresponding_batches[key][:, 1], corresponding_batches[key][:, 0],
+            corresponding_batches[key][:, 3], '-o', color=colors[key])
+
+plt.show()
