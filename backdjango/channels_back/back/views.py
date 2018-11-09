f9d73d8ab7a5cc8f8d1dc5874ba2535dfaee8202
Hadrien Lucas <hadrien.lucas@student.ecp.fr>
2018-11-09 15:51:05 +0100

    Added upload and storage of prp files

--- backdjango/channels_back/back/views.py
+++ backdjango/channels_back/back/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from channels import Group
 import json
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from .forms import FileForm
+from django.core.files.storage import FileSystemStorage
+
+
 
 
 def user_list(request):
@@ -31,5 +37,24 @@ def test(request):
         })
     })
     return render(request, 'back/user_list.html')
-    
+
+
+@csrf_exempt#makes a security exception for this function to be triggered
+def upload(request):
+    """
+        Deals with the upload and save of a PRP file so that the user can upload his own scenario
+        :return: success if the file is safe and sound
+    """
+    global path
+    if(request.method=='POST'):
+        form=FileForm(request.POST, request.FILES)
+        if form.is_valid():
+            global_file=request.FILES['File']
+            fs=FileSystemStorage()
+            filename=fs.save(global_file.name,global_file)
+            path=fs.location+'/'+filename
+        return(HttpResponse('<h1>Page was found</h1>'))
+    else:
+        return(HttpResponse('<h1>Page was not found</h1>'))
+
 
