207465230dc2c8b77fb2ba75267eb0faeba4dc7d
Pierre Averous <piaverous@gmail.com>
2018-12-04 13:23:16 +0100

    repaired dashed lines

--- front/src/components/Map/PotentialLines.js
+++ front/src/components/Map/PotentialLines.js
@@ -4,20 +4,22 @@ import { Feature, Layer } from "react-mapbox-gl";
 class PotentialLines extends Component {
     render() {
         console.log(this.props);
-        return (<Layer
-            type="line"
-            id={"potential_lines" + this.props.network}
-            key={"potential_lines" + this.props.network}
-            paint={{
-                "line-color": "grey",
-                "line-dasharray": [5]
-            }}>
-            {this.props.links.map((link, keyy) => {
-                return (<Feature coordinates={link} key={keyy}></Feature>);
-            })
-            }
-            )}
-        </Layer>);
+        return (
+            <Layer
+                type="line"
+                id={"potential_lines" + this.props.network}
+                key={"potential_lines" + this.props.network}
+                paint={{
+                    "line-color": "grey",
+                    "line-dasharray": [10, 5]
+                }}>
+                {
+                    this.props.links.map((link, keyy) =>
+                        <Feature coordinates={link} key={keyy}></Feature>
+                    )
+                }
+            </Layer>
+        );
     }
 }
 
