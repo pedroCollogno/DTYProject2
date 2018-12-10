88cab9342f47296d7b3c7f6ba251925095d53aef
Pierre Averous <piaverous@gmail.com>
2018-12-10 12:15:23 +0100

    Updated CPU measuring

--- ml_tools/src/profile_thread.py
+++ ml_tools/src/profile_thread.py
@@ -52,7 +52,7 @@ class ProfilerThread(threading.Thread):
         for process in self.processes:
             memory_info = process.memory_info().rss
             memory_percent = process.memory_percent()
-            cpu_percent = sum([perc if perc <= 100 else 100 for perc in process.cpu_percent(percpu=True)])/cpu_count
+            cpu_percent = process.cpu_percent()/cpu_count if process.cpu_percent()/cpu_count <= 100 else 100 
 
             if memory_info > 0:
                 total_mem_info += memory_info
