0b59993b9088dedc8d9e13b2ee8daa34e6a28c0f
arturo <arthur.goutallier@student.ecp.fr>
2018-12-13 17:12:01 +0100

    Doc model.py

--- ml_tools/src/deep_learning/model.py
+++ ml_tools/src/deep_learning/model.py
@@ -54,9 +54,9 @@ def fake_data(sequence_size, all=True, n_samples=1000, equalize=False):
     """ Generates fake data
 
     :param sequence_size: the size of the sequence to generate
-    :param all: (optional) TODO: COMPLETE DOCS 
-    :param n_samples: (optional) TODO: COMPLETE DOCS 
-    :param equalize: (optional) TODO: COMPLETE DOCS 
+    :param all: (optional) Boolean, if we want to generate all the possible combinations samples (be careful, goes in 2**sequence_size)
+    :param n_samples: (optional) Number of samples generated if all = False
+    :param equalize: (optional) Boolean, if we want to create as many 0 label as 1 label
     """
     # If we want all the possible combinations of size sequence_size (be careful this goes in 2**sequence_size)
     if all:
@@ -101,15 +101,16 @@ def train():
     """ 
         Trains a model on data produced with fake_data function. Saves weights to './weights' folder
     """
+
+    # Training is done on sequence size 50
     data = fake_data(50, all=False, equalize=False)
 
     X = np.array(data[0])
     Y = np.array(data[1])
 
+    # The model of our neural network
     model = Sequential()
     model.add(LSTM(units=128, input_shape=(None, 2)))
-    #model.add(LSTM(units = 128, input_shape=(None , 2)))
-    #model.add(Dense(2, activation='relu'))
     model.add(Dense(1, activation="sigmoid"))
 
     callback_dir = os.path.join(LOG_DIR, time())
@@ -118,8 +119,6 @@ def train():
 
     model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=[
                   'accuracy', auc_roc, f1_score_threshold(), precision_threshold(), recall_threshold()])
-
-    # model.summary()
 
     model.fit(
         X,
@@ -138,8 +137,9 @@ def train():
 
 def test(file_name):
     """
-        Tests stuff
-        TODO: COMPLETE DOCS 
+        Pass the data (processed by processData in processDL.py) into the model
+        Each comparaison between two emittors gives a score, all the scores are stored in a .pkl file
+        :param file_name: Name of the folder where the preprocessed data is stored (see processDL.py), in the /pkl folder
     """
 
     file_path = os.path.join(PKL_DIR, file_name)
@@ -151,6 +151,7 @@ def test(file_name):
     pbar = ProgressBar(maxval=(len(filelist)))
     pbar.start()
 
+    # Get all the files from the folder (each dataframe stored as .pkl file can contain max. 2000 lines (see processDL.py))
     logger.info("Getting the files...")
     for f in filelist:
         df_temp = pd.read_pickle(os.path.join(file_path, f))
@@ -164,15 +165,15 @@ def test(file_name):
     Y = np.array(list(df['Y'].values))
     id_Couple = (list(df['id_Couple'].values))
 
+
     model = Sequential()
     model.add(LSTM(units=128, input_shape=(None, 2)))
-    #model.add(Dense(2, activation='relu'))
     model.add(Dense(1, activation="sigmoid"))
     model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=[
                   'accuracy', f1_score_threshold(), precision_threshold(), recall_threshold()])
-    model.load_weights(os.path.join(WEIGHTS_DIR, 'my_model_weights.h5'))
 
-    #ones_index = np.where(Y==1)[0]
+    # Loads the pretrained wieghts
+    model.load_weights(os.path.join(WEIGHTS_DIR, 'my_model_weights.h5'))
 
     X_new = []
     Y_new = []
@@ -180,12 +181,13 @@ def test(file_name):
     pbar2 = ProgressBar(maxval=len(X))
     pbar2.start()
     logger.info("Passing some data in the first network...")
-    #random_indexs = random.sample(range(0, len(X)), 50)
+    # For all couples 
     for itemindex in range(len(X)):
         X_test = X[itemindex]
         Y_test = Y[itemindex]
         Y_new.append(Y_test)
 
+        # We feed the network with sequence size 50, so we need to 'cut' the whole sequence into several 50-long sequences
         number_steps = int(len(X_test)/50)
         to_predict = []
         for i in range(number_steps):
@@ -206,6 +208,7 @@ def test(file_name):
             'id_Couple': id_Couple
         }, columns=['X_new', 'Y_new', 'id_Couple'])
 
+    # Store the result data (as X_new), Y_new = Y and id_couple in /pkl2 folder
     name = '{}.pkl'.format(file_name)
     create_new_folder('pkl2', DATA_DIR)
     file_to_save_path = os.path.join(PKL2_DIR, name)
@@ -216,16 +219,11 @@ def test(file_name):
 
     logger.debug(df)
 
-    # predictions = []
-    # for item in to_predict:
-    #     predictions.append(model.predict(item))
-    # print(predictions, predictions.count(1))
-
-
 def train2(file_name):
     """ 
-        Trains a model on data from '.pkl2' folder
-        TODO: COMPLETE DOCS 
+        Uses the data output by the neural network (with test()) and creates the clusters of emittors
+        :param file_name: Name of the data file from the /pkl2 folder to use 
+        :return: A list of all the predictions and all the emittors ids
     """
 
     name = '{}.pkl'.format(file_name)
@@ -233,26 +231,11 @@ def train2(file_name):
 
     df = pd.read_pickle(file_path)
 
-    # model2 = Sequential()
-    # model2.add(LSTM(units=128, input_shape=(None, 1)))
-    # model2.add(Dense(1, activation="sigmoid"))
-
     # Get the data
 
     X = np.array(list(df['X_new'].values))
     Y = np.array(list(df['Y_new'].values))
     id_Couple = list(df['id_Couple'].values)
-
-    # Optional : Balances the 0 and 1
-
-    nb_ones = np.count_nonzero(Y == 1)
-    zeros_index = np.where(Y == 0)[0]
-    df = df.drop(df.index[zeros_index[nb_ones:]])
-
-    df = df.sample(frac=1)
-
-    X_new = np.array(list(df['X_new'].values))
-    Y_new = np.array(list(df['Y_new'].values))
 
     # How many 0 or 1
 
@@ -297,175 +280,22 @@ def train2(file_name):
     logger.debug(normalized_distance_matrix)
 
     np_data = np.array(normalized_distance_matrix)
-    # print(np_data)
+
+    # Use DBSCAN to predict the clusters
     prediction = DBSCAN(eps=0.0000002, min_samples=1,
                         metric="precomputed").fit_predict(np_data)
 
     # If we want to use the tresholds method
     threshold = False
     if threshold:
-
+        thresholdNumber = 10e10
         Y_predicted = []
         for i in range(len(X)):
             predict = 1
             #product = np.prod(X_new[i])
             mean = np.divide(1, np.square(X[i])).mean()
-            if mean > 10e10:
+            if mean > thresholdNumber:
                 predict = 0
             Y_predicted.append(predict)
-
-        createNetworks(Y, np.array(Y_predicted), id_Couple)
-
-        plotCompareThresholds(X, Y)
 
     return(prediction, list(index))
-
-
-def plotCompareThresholds(X, Y):
-    thresholds = [10e3, 10e4, 5*10e4, 10e5, 5*10e5, 10e6, 2*10e6, 5*10e6, 6*10e6,
-                  7*10e6, 8*10e6, 9*10e6, 10e7, 2*10e7, 5*10e7, 10e8, 10e9, 10e10, 10e30]
-    precision0 = []
-    precision1 = []
-    recall0 = []
-    recall1 = []
-    fscore0 = []
-    fscore1 = []
-    for threshold in thresholds:
-        test = []
-        Y_predicted = []
-        for i in range(len(X)):
-            predict = 1
-            #product = np.prod(X_new[i])
-            mean = np.divide(1, np.square(X[i])).mean()
-            if mean > threshold:
-                predict = 0
-            Y_predicted.append(predict)
-            test.append(abs(predict - Y[i]))
-        score = skmetrics.precision_recall_fscore_support(Y, Y_predicted)
-        #print ("Precision '1' :", score[0][1]," Recall '1' :", score[1][1], " Fscore '1' :", score[2][1], " Number of samples '1' :", score[3][1])
-        #print ("Precision '0' :", score[0][0]," Recall '0' :", score[1][0], " Fscore '0' :", score[2][0], " Number of samples '0' :", score[3][0])
-        precision0.append(score[0][0])
-        precision1.append(score[0][1])
-        recall0.append(score[1][0])
-        recall1.append(score[1][1])
-        fscore0.append(score[2][0])
-        fscore1.append(score[2][1])
-
-    # Plot
-    ax1 = plt.subplot(311)
-    ax1.set_xscale('log')
-    plt.plot(thresholds, precision0, 'r')
-    plt.plot(thresholds, precision1, 'b')
-    plt.ylabel('Threshold')
-    plt.xlabel('Precision')
-    ax2 = plt.subplot(312, sharex=ax1, sharey=ax1)
-    plt.plot(thresholds, recall0, 'r')
-    plt.plot(thresholds, recall1, 'b')
-    plt.ylabel('Threshold')
-    plt.xlabel('Recall')
-    ax3 = plt.subplot(313, sharex=ax1, sharey=ax1)
-    plt.plot(thresholds, fscore0, 'r')
-    plt.plot(thresholds, fscore1, 'b')
-    plt.ylabel('Threshold')
-    plt.xlabel('Fscore')
-    plt.legend()
-    plt.show()
-
-
-def createNetworks(Y, Y_predicted, id_Couple, allLinks=False):
-
-    if allLinks:
-
-        id1 = [couple[0] for couple in id_Couple]
-        id2 = [couple[1] for couple in id_Couple]
-        df = pd.DataFrame(
-            {
-                'id1': id1,
-                'id2': id2,
-                'link': Y_predicted
-            }, columns=['id1', 'id2', 'link']
-        )
-        logger.debug(df)
-
-        fig, ax = plt.subplots()
-        G = nx.from_pandas_edgelist(df, 'id1', 'id2', edge_attr='link')
-        durations = [i['link'] for i in dict(G.edges).values()]
-
-        pos = nx.spring_layout(G)
-
-        color_names = ['r', 'b']
-        colors = [color_names[i['link']] for i in dict(G.edges).values()]
-        logger.debug(colors)
-
-        nx.draw_networkx_nodes(G, pos, ax=ax, labels=True)
-        nx.draw_networkx_edges(G, pos, edge_color=colors, ax=ax)
-        _ = nx.draw_networkx_labels(G, pos, ax=ax)
-
-        plt.show()
-
-    else:
-
-        fig, axes = plt.subplots(nrows=1, ncols=2)
-        ax = axes.flatten()
-
-        # Draw the graph for predicted data
-
-        links_id1_predicted = []
-        links_id2_predicted = []
-        ones_index_predicted = np.where(Y_predicted == 1)[0]
-        for index in ones_index_predicted:
-            links_id1_predicted.append(id_Couple[index][0])
-            links_id2_predicted.append(id_Couple[index][1])
-        df = pd.DataFrame(
-            {
-                'Id1': links_id1_predicted,
-                'Id2': links_id2_predicted
-            }, columns=['Id1', 'Id2'])
-
-        df_predicted = pd.crosstab(df.Id1, df.Id2)
-        idx_predicted = df_predicted.columns.union(df_predicted.index)
-        df_predicted = df_predicted.reindex(
-            index=idx_predicted, columns=idx_predicted, fill_value=0)
-
-        G_predicted = nx.from_pandas_adjacency(df_predicted)
-        pos_predicted = nx.spring_layout(G_predicted)
-
-        nx.draw_networkx(
-            G_predicted, ax=ax[0], pos=pos_predicted, with_labels=True)
-        ax[0].set_axis_off()
-
-        # Draw the graph for the true data
-
-        links_id1_true = []
-        links_id2_true = []
-        ones_index_true = np.where(Y == 1)[0]
-        for index in ones_index_true:
-            links_id1_true.append(id_Couple[index][0])
-            links_id2_true.append(id_Couple[index][1])
-        df = pd.DataFrame(
-            {
-                'Id1': links_id1_true,
-                'Id2': links_id2_true
-            }, columns=['Id1', 'Id2'])
-
-        df_true = pd.crosstab(df.Id1, df.Id2)
-        idx_true = df_true.columns.union(df_true.index)
-        df_true = df_true.reindex(
-            index=idx_true, columns=idx_true, fill_value=0)
-
-        G_true = nx.from_pandas_adjacency(df_true)
-        pos_true = nx.spring_layout(G_true)
-
-        nx.draw_networkx(G_true, ax=ax[1], pos=pos_true, with_labels=True)
-        ax[1].set_axis_off()
-
-        # Plot both
-
-        plt.show()
-
-
-"""This part runs if you run 'python model.py file_name' in the console
-    :param 1: name of file in /pkl folder
-"""
-if __name__ == '__main__':
-    train2(sys.argv[1])
